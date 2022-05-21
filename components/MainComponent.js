73265d10ceae4f827c13f979b6d1e20078477607
srhodes <amplifier339@comcast.net>
2022-05-20 23:18:19 -0700

    forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,7 @@ import { createAppContainer } from 'react-navigation'
 import { Icon } from 'react-native-elements'
 import About from './AboutComponent'
 import Contact from './ContactComponent'
+import Reservation from './ReservationComponent'
 import SafeAreaView from 'react-native-safe-area-view'
 import { connect } from 'react-redux'
 import { fetchCampsites, fetchComments, fetchPromotions,
@@ -120,6 +121,29 @@ const ContactNavigator = createStackNavigator(
     }  
 )
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })  
+    }  
+)
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -162,6 +186,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />    
+                )
+            } 
+        },
+        Reservation : { 
+            screen: ReservationNavigator, 
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
